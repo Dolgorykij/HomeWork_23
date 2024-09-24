1240a73296b7a6232b0408163f80c03cccf44ab0
Dolgorykij <dolgo-rykij@mail.ru>
2024-09-25 02:53:44 +0300

    initial

---
+++ src/main/java/HomeWork/HomeWork_23/HomeWork23Application.java
@@ -0,0 +1,13 @@
+package HomeWork.HomeWork_23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWork23Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeWork23Application.class, args);
+	}
+
+}
---
+++ src/test/java/HomeWork/HomeWork_23/HomeWork23ApplicationTests.java
@@ -0,0 +1,13 @@
+package HomeWork.HomeWork_23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWork23ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
